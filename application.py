32f424222370a83c751e807875c51d01338810e2
Russel Caballero <caballerorussel@yahoo.com>
2020-08-20 01:32:51 -0700

    Second commit of my awesome company

--- application.py
+++ application.py
@@ -6,12 +6,12 @@ def say_hello(username = "World"):
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>Sel</title> </head>\n<body>'''
 instructions = '''
     <p>
-    <h1 style="background-color: red; color: white"> Hello People! </h1>
+    <h1 style="background-color: red; color: white"> Hello people!(my first changes inside terminal and allowing Elasticbeanstalk do it for me) </h1>
     </br>
-    <h1>Imagine something really fun here! XD </h1>
+    <h1 style="background-color: blue; color: white"> Just imagine that something really fun in here! (my second changes inside of terminal just using 1 command "eb deploy") </h1>
     <hr/>
     <em>Hint</em>: This is a RESTful web service! Append a username
     to the URL (for example: <code>/Thelonious</code>) to say hello to
